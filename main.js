f0e4d632c523293ad62e906ed2262f78c194282d
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-02 04:26:28 -0400

    complete createURL function

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ import { publicKey, privateKey } from "./key";
 
 const apiBaseURL = "https://gateway.marvel.com/v1/public";
 
+// Function that creates the URL that takes a parameter to search
+function createURL(search) {
+  const ts = Date.now();
+  const params = new URLSearchParams({
+    ts: ts,
+    apikey: publicKey,
+    hash: md5(ts + privateKey + publicKey),
+    nameStartsWith: search,
+    limit: 100,
+  });
+  const endpoint = `${apiBaseURL}/characters?`; // Notice the question mark to start the query parameters.
+  const url = endpoint + params;
+  return url;
+}
+
 // NOTE: Example characters with INVALID images: Spider-dok, Blue Marvel, Revanche, Unus
 const withValidImages = (character) =>
   character.thumbnail.path.includes("image_not_available") === false &&
