3daefb4b389e84cc5c1e3b659ad9ed49449963e4
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-02 04:20:08 -0400

    valid image function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import { publicKey, privateKey } from "./key";
 
 const apiBaseURL = "https://gateway.marvel.com/v1/public";
+
+// NOTE: Example characters with INVALID images: Spider-dok, Blue Marvel, Revanche, Unus
+const withValidImages = (character) =>
+  character.thumbnail.path.includes("image_not_available") === false &&
+  character.thumbnail.path.includes("4c002e0305708") === false; // 4c002e0305708.gif is an "image not found" thumbnail
