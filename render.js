995f207f6026e03ecdf0848351271c785f35bc1d
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-03 10:00:30 -0400

    create toComicCard Function

--- render.js
+++ render.js
@@ -5,4 +5,18 @@ function renderComics(comics) {
   comicsContainer.replaceChildren(...comicCards); // replaceChildren() is our alternative to append() when we want to replace the contents of the parent element, instead of adding new elements
 }
 
+function toComicCard(comic) {
+  const imgURL = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+  const comicImg = document.createElement("img");
+  comicImg.src = imgURL;
+
+  const comicCard = document.createElement("div");
+  comicCard.classList.add("comic-card");
+
+  comicCard.append(comicImg);
+  comicCard.append(comicTitleName(comic));
+
+  return comicCard;
+}
+
 export { renderComics };
