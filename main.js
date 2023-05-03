4de67613b079e816d81e88fb00ad05b03b6d3d0a
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-02 22:48:30 -0400

    created searchEvent Function and Modulization

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ import { publicKey, privateKey } from "./key.js";
 
 const apiBaseURL = "https://gateway.marvel.com/v1/public";
 
+// NOTE: Example characters with INVALID images: Spider-dok, Blue Marvel, Revanche, Unus
+const withValidImages = (comics) =>
+  comics.thumbnail.path.includes("image_not_available") === false &&
+  comics.thumbnail.path.includes("4c002e0305708") === false; // 4c002e0305708.gif is an "image not found" thumbnail
+
+function withValidPrices(comics) {
+  const hasValidPrices = comics.prices.every(
+    (price) => !price.price.toString().includes("0")
+  );
+  return hasValidPrices;
+}
+
 // Function that creates the URL that takes a parameter to search
 function createURL(search) {
   const ts = Date.now();
@@ -25,21 +37,11 @@ function searchMarvelAPI(search) {
       console.log(body.data.results);
       const matchedComics = body.data.results;
       const comicsWithImages = matchedComics.filter(withValidImages);
-      console.log(body.data.results[0].prices[0]);
+      //   console.log(body.data.results[0].prices[0]);
       const comicsWithPrices = comicsWithImages.filter(withValidPrices);
-      console.log(comicsWithPrices);
+      //   console.log(comicsWithPrices);
     });
 }
 
 searchMarvelAPI("spider");
-// NOTE: Example characters with INVALID images: Spider-dok, Blue Marvel, Revanche, Unus
-const withValidImages = (comics) =>
-  comics.thumbnail.path.includes("image_not_available") === false &&
-  comics.thumbnail.path.includes("4c002e0305708") === false; // 4c002e0305708.gif is an "image not found" thumbnail
-
-function withValidPrices(comics) {
-  const hasValidPrices = comics.prices.every(
-    (price) => !price.price.toString().includes("0")
-  );
-  return hasValidPrices;
-}
+export { searchMarvelAPI };
---
+++ searchEvent.js
@@ -0,0 +1,14 @@
+import { searchMarvelAPI } from "./marvelService.js";
+const DEFAULT_SEARCH_TERM = "a`";
+
+// Run a search once on page load (before the user gets a chance to enter a new search term),
+// so that the page starts with some characters already displayed:
+searchMarvelAPI(DEFAULT_SEARCH_TERM);
+
+// Handle search form submissions...
+document.querySelector("#search-form").addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the form from doing its default behavior of refreshing the page.
+
+  const searchInputElement = event.target.elements.search;
+  searchMarvelAPI(searchInputElement.value || DEFAULT_SEARCH_TERM);
+});
