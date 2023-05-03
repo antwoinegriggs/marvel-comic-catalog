12f02246fc736972b2a02d6946999d4627081ec2
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-03 10:03:38 -0400

    create comicTitleName Function

--- render.js
+++ render.js
@@ -19,4 +19,31 @@ function toComicCard(comic) {
   return comicCard;
 }
 
+function comicTitleName(comic) {
+  const link = document.createElement("a");
+  link.alt = comic.name + " on Marvel.com";
+  link.target = "_blank";
+
+  const urlObject =
+    comic.urls.find((urlObject) => urlObject.type === "wiki") || // or backup links if there is no wiki link for this character...
+    comic.urls.find((urlObject) => urlObject.type === "detail") ||
+    comic.urls.find((urlObject) => urlObject.type === "comiclink");
+
+  link.href = urlObject.url;
+
+  const firstLineElement = document.createElement("div");
+  const secondLineElement = document.createElement("div");
+
+  firstLineElement.classList.add("name-first-line");
+  secondLineElement.classList.add("name-second-line");
+
+  const [firstLine, secondLine] = comic.title.split(" ");
+
+  firstLineElement.append(firstLine);
+  if (secondLine) secondLineElement.append("(" + secondLine); // Don't add content to this DIV if there is no text in parentheses (see the split() above)
+
+  link.append(firstLineElement, secondLineElement);
+
+  return link;
+}
 export { renderComics };
