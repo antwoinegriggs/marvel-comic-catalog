6cd45d8fc29c1d5f8bf615dd88dc21c4df945db0
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-03 20:31:17 -0400

    add comments to code

--- main.js
+++ main.js
@@ -3,46 +3,65 @@ import { renderComics } from "./render.js";
 
 const apiBaseURL = "https://gateway.marvel.com/v1/public";
 
-// NOTE: Example characters with INVALID images: Spider-dok, Blue Marvel, Revanche, Unus
+// Returns true if the comic has a valid image
 const withValidImages = (comics) =>
   comics.thumbnail.path.includes("image_not_available") === false &&
   comics.thumbnail.path.includes("4c002e0305708") === false; // 4c002e0305708.gif is an "image not found" thumbnail
 
+// Returns true if the comic has valid prices
 function withValidPrices(comics) {
+  // Check if every price in the comic has a non-zero price
   const hasValidPrices = comics.prices.every(
     (price) => !price.price.toString().includes("0")
   );
   return hasValidPrices;
 }
 
-// Function that creates the URL that takes a parameter to search
+// Creates a URL for searching Marvel API for comics with titles starting with a given search term
 function createURL(search) {
+  // Get the current timestamp
   const ts = Date.now();
+
+  // Create a new URLSearchParams object and set the necessary query parameters
   const params = new URLSearchParams({
     ts: ts,
     apikey: publicKey,
-    hash: md5(ts + privateKey + publicKey),
+    hash: md5(ts + privateKey + publicKey), // Generate hash for authentication
     titleStartsWith: search,
-    limit: 100,
+    limit: 100, // Set the maximum number of results to be returned
   });
+
+  // Construct the endpoint URL for searching comics with the query parameters
   const endpoint = `${apiBaseURL}/comics?`; // Notice the question mark to start the query parameters.
+
+  // Combine the endpoint URL with the query parameters to form the complete API request URL
   const url = endpoint + params;
+
+  // Return the complete API request URL
   return url;
 }
 
 function searchMarvelAPI(search) {
+  // Create URL for the Marvel API search
   const url = createURL(search);
+
+  // Fetch data from the Marvel API using the URL
   fetch(url)
+    // Parse response as JSON
     .then((response) => response.json())
+    // Extract comic results from response body
     .then((body) => {
       console.log(body.data.results);
       const matchedComics = body.data.results;
+
+      // Filter comics with valid images
       const comicsWithImages = matchedComics.filter(withValidImages);
-      //   console.log(body.data.results[0].prices[0]);
+
+      // Filter comics with valid prices
       const comicsWithPrices = comicsWithImages.filter(withValidPrices);
       console.log(comicsWithPrices);
 
-      //Render Function
+      // Render filtered comics using the renderComics function
       renderComics(comicsWithPrices);
     });
 }
--- render.js
+++ render.js
@@ -1,49 +1,74 @@
 const comicsContainer = document.querySelector("#comic-container");
 
+// Renders an array of comics as a list of comic cards in the DOM
 function renderComics(comics) {
+  // Map the array of comics to an array of comic card elements
   const comicCards = comics.map(toComicCard);
-  comicsContainer.replaceChildren(...comicCards); // replaceChildren() is our alternative to append() when we want to replace the contents of the parent element, instead of adding new elements
+
+  // Replace the contents of the comicsContainer element with the array of comic card elements
+  comicsContainer.replaceChildren(...comicCards);
+  // Note: replaceChildren() is our alternative to append() when we want to replace the contents of the parent element, instead of adding new elements
 }
 
+// Creates a new comic card element from a given comic object and returns it
 function toComicCard(comic) {
+  // Construct the URL for the comic's thumbnail image using its path and extension
   const imgURL = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+
+  // Create a new image element with the constructed URL
   const comicImg = document.createElement("img");
   comicImg.src = imgURL;
 
+  // Create a new div element to hold the comic card contents and apply a class for styling
   const comicCard = document.createElement("div");
   comicCard.classList.add("comic-card");
 
+  // Append the comic image and title to the comic card div element
   comicCard.append(comicImg);
   comicCard.append(comicTitleName(comic));
 
+  // Return the newly created comic card element
   return comicCard;
 }
 
+// Returns a new link element that displays the comic title and links to its page on Marvel.com
 function comicTitleName(comic) {
+  // Create a new link element with alt text and a target
   const link = document.createElement("a");
   link.alt = comic.name + " on Marvel.com";
   link.target = "_blank";
 
+  // Find the correct URL object for the comic's page on Marvel.com based on its type
   const urlObject =
-    comic.urls.find((urlObject) => urlObject.type === "wiki") || // or backup links if there is no wiki link for this character...
-    comic.urls.find((urlObject) => urlObject.type === "detail") ||
-    comic.urls.find((urlObject) => urlObject.type === "comiclink");
+    comic.urls.find((urlObject) => urlObject.type === "wiki") || // try to find a wiki link first
+    comic.urls.find((urlObject) => urlObject.type === "detail") || // then try a detail link
+    comic.urls.find((urlObject) => urlObject.type === "comiclink"); // or a comiclink link as a last resort
 
+  // Set the link's href to the URL of the comic's page on Marvel.com
   link.href = urlObject.url;
 
+  // Create two div elements to hold the comic title text, with classes for styling
   const firstLineElement = document.createElement("div");
   const secondLineElement = document.createElement("div");
-
   firstLineElement.classList.add("name-first-line");
   secondLineElement.classList.add("name-second-line");
 
+  // Split the comic title into two parts, the first line and the second line (if present)
   const [firstLine, secondLine] = comic.title.split(" ");
 
+  // Append the first line of the title to the first line div element
   firstLineElement.append(firstLine);
-  if (secondLine) secondLineElement.append("(" + secondLine); // Don't add content to this DIV if there is no text in parentheses (see the split() above)
 
+  // If there is a second line of the title (in parentheses), append it to the second line div element
+  if (secondLine) {
+    secondLineElement.append("(" + secondLine);
+  }
+
+  // Append the first and second line div elements to the link element
   link.append(firstLineElement, secondLineElement);
 
+  // Return the newly created link element with the comic title text and URL
   return link;
 }
+
 export { renderComics };
