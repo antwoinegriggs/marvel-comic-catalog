b81f397e5138ca9764cd10e2e77d4cd441406650
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-05 08:47:29 -0400

    create PATCH and DELETE request

--- cartRender.js
+++ cartRender.js
@@ -19,6 +19,7 @@ const multiply = (x, y) => Number(x) * Number(y);
 function toCartCard(cart) {
   const cartCard = document.createElement("div");
   cartCard.classList.add("cart-card");
+  cartGallery.append(cartCard);
 
   const comicTitle = document.createElement("div");
   comicTitle.classList.add("cart-comic-title");
@@ -32,11 +33,48 @@ function toCartCard(cart) {
   console.log(comicPrice);
   cartCard.append(comicPrice);
 
-  const comicQuantity = document.createElement("div");
-  comicQuantity.classList.add("cart-comic-quantity");
-  comicQuantity.textContent = cart.quantity;
-  console.log(comicQuantity);
-  cartCard.append(comicQuantity);
+  const editCart = document.createElement("div");
+  editCart.classList.add("edit-cart");
+  cartCard.append(editCart);
+
+  // Create a subtract button and append it to the cart options div
+  const subBtn = document.createElement("button");
+  subBtn.classList.add("sub-cart-btn");
+  subBtn.textContent = "-";
+  editCart.append(subBtn);
+  subBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    cart.quantity -= 1;
+    cartQuantity.textContent = cart.quantity;
+    patchRequest(cart);
+  });
+
+  const cartQuantity = document.createElement("div");
+  cartQuantity.classList.add("cart-comic-quantity");
+  cartQuantity.textContent = cart.quantity;
+  console.log(cartQuantity);
+  editCart.append(cartQuantity);
+
+  // Create an add button and append it to the cart options div
+  const addBtn = document.createElement("button");
+  addBtn.classList.add("add-cart-btn");
+  editCart.append(addBtn);
+  addBtn.textContent = "+";
+  addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    cart.quantity += 1;
+    cartQuantity.textContent = cart.quantity;
+    patchRequest(cart);
+  });
+
+  const removeCart = document.createElement("button");
+  removeCart.classList.add("remove-cart-btn");
+  removeCart.textContent = "Remove";
+  cartCard.append(removeCart);
+  removeCart.addEventListener("click", (e) => {
+    cartCard.remove();
+    deleteRequest(cart);
+  });
 
   const subtotal = document.createElement("div");
   subtotal.classList.add("subtotal");
@@ -45,9 +83,56 @@ function toCartCard(cart) {
   subtotal.textContent = multiply(cart.price, cart.quantity);
   console.log(subtotal);
   cartCard.append(subtotal);
-  cartGallery.append(cartCard);
 
   console.log(totalCart);
   return cartCard;
 }
 console.log(totalCart);
+
+function patchRequest(cart) {
+  // Make the POST request using fetch()
+  fetch(`http://localhost:3000/cart/${cart.id}`, {
+    method: "PATCH",
+    body: JSON.stringify(cart),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+  console.log("success");
+}
+
+function deleteRequest(cart) {
+  // Make the POST request using fetch()
+  fetch(`http://localhost:3000/cart/${cart.id}`, {
+    method: "DELETE",
+    //   body: JSON.stringify(cart),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+  console.log("success");
+}
